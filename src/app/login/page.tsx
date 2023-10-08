63c3a4416e96f834fe836c1ad8530d5c75120b73
hichamelamrani <139113583+hichamelamrani@users.noreply.github.com>
2023-10-08 15:00:40 +0100

    add some code in page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,40 @@
+import Link from "next/link";
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="m-8 flex items-center justify-center">
+      <div className="w-fite p-6 rounded-sm shadow-sm bg-blue-100">
+        <h1 className="w-6/12 pb-2 text-2xl border border-b-gray-300">
+          Signup
+        </h1>
+        <form className="mt-6 flex flex-col gap-4">
+          <input
+            className="p-3 rounded-sm"
+            type="text"
+            placeholder="Username"
+          />
+
+          <input
+            className="p-3 rounded-sm"
+            type="password"
+            placeholder="Password"
+          />
+          <div className="mt-4 flex items-center justify-between gap-4">
+            <button
+              className="font-bold bg-blue-500 text-white p-2 rounded-sm"
+              type="submit"
+            >
+              Signup
+            </button>
+            <Link href="/login">
+              Already have an account? <span className="font-bold">Login</span>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default page;
