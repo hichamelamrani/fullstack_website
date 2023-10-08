cde455fa6e3a30f315e31675dbda7dad477bcbb5
hichamelamrani <139113583+hichamelamrani@users.noreply.github.com>
2023-10-08 14:48:47 +0100

    opetmaisem page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,49 @@
-"use client";
-import React from "react";
+import React, { useState, useRef } from "react";
 import Link from "next/link";
 
-export default function Login() {
-  const [user, setUser] = React.useState({
+const Login = () => {
+  const [user, setUser] = useState({
     username: "",
     password: "",
   });
 
-  const handelLogin = (e: React.FormEvent) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleLogin = (e) => {
     e.preventDefault();
     console.log(user);
   };
+
+  const handleUsernameChange = (e) => {
+    setUser((prevUser) => ({ ...prevUser, username: e.target.value }));
+  };
+
+  const handlePasswordChange = (e) => {
+    setUser((prevUser) => ({ ...prevUser, password: e.target.value }));
+  };
+
   return (
     <div className="m-8 flex items-center justify-center">
       <div className="w-fite p-6 rounded-sm shadow-sm bg-blue-100">
         <h1 className="w-6/12 pb-2 text-2xl border border-b-gray-300">Login</h1>
-        <form className="mt-6 flex flex-col gap-4" onSubmit={handelLogin}>
+        <form className="mt-6 flex flex-col gap-4" onSubmit={handleLogin}>
           <input
+            ref={usernameRef}
             className="p-3 rounded-sm"
             type="text"
             placeholder="Username"
             value={user.username}
-            onChange={(e) => setUser({ ...user, username: e.target.value })}
+            onChange={handleUsernameChange}
           />
 
           <input
+            ref={passwordRef}
             className="p-3 rounded-sm"
             type="password"
             placeholder="Password"
             value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            onChange={handlePasswordChange}
           />
           <div className="mt-4 flex items-center justify-between gap-4">
             <button
@@ -39,7 +52,7 @@ export default function Login() {
             >
               Login
             </button>
-            You don't have account?
+            You don't have an account?
             <Link className="font-bold" href="/signup">
               Signup
             </Link>
@@ -48,4 +61,6 @@ export default function Login() {
       </div>
     </div>
   );
-}
+};
+
+export default Login;
