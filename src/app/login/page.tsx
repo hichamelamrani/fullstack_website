39eee5a924f4350aff29a18d8baddedcb4598bbe
hichamelamrani <139113583+hichamelamrani@users.noreply.github.com>
2023-10-08 15:04:06 +0100

    add useState hock in page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 import React from "react";
 
 function page() {
+  const [user, setuser] = React.useState({
+    username: "",
+    password: "",
+  });
   return (
     <div className="m-8 flex items-center justify-center">
       <div className="w-fite p-6 rounded-sm shadow-sm bg-blue-100">
-        <h1 className="w-6/12 pb-2 text-2xl border border-b-gray-300">
-          Signup
-        </h1>
+        <h1 className="w-6/12 pb-2 text-2xl border border-b-gray-300">Login</h1>
         <form className="mt-6 flex flex-col gap-4">
           <input
             className="p-3 rounded-sm"
@@ -27,8 +29,8 @@ function page() {
             >
               Signup
             </button>
-            <Link href="/login">
-              Already have an account? <span className="font-bold">Login</span>
+            <Link href="/signup">
+              You don't have account? <span className="font-bold">Signup</span>
             </Link>
           </div>
         </form>
